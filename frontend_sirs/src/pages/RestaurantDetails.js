b37a48d9f599c7078de6ce8e21b43c0a96deabbc
PedroPereir4 <pedrosergiohortapereira@gmail.com>
2022-12-27 12:46:36 +0000

    Frontend cookin

--- frontend_sirs/src/pages/RestaurantDetails.js
+++ frontend_sirs/src/pages/RestaurantDetails.js
@@ -1,19 +1,50 @@
 import { React, useState } from "react";
 import { useParams } from "react-router-dom";
-import {HvCheckBox, HvContainer, HvInput} from "@hitachivantara/uikit-react-core";
+import {
+    HvButton,
+    HvCheckBox,
+    HvContainer,
+    HvDatePicker, HvDialog, HvDialogActions, HvDialogContent, HvDialogTitle,
+    HvInput,
+    HvTimePicker
+} from "@hitachivantara/uikit-react-core";
 
 export const RestaurantDetails = () => {
-    const { name } = useParams()
-    const [numberPeople, setPeople] = useState(0)
+    const { name } = useParams();
+    const [numberPeople, setPeople] = useState(0);
+    const [showSuccess, setSuccess] = useState(false);
+    const [cardPoints, setCardPoints] = useState(15);
+    const [useDiscount, setDiscount] = useState(false);
+    const [date, setDate] = useState(false);
+    const [time, setTime] = useState(false);
 
     const validationMessages = {
         requiredError: "The number is required",
         maxCharError: "Number is too big",
         typeMismatchError: "Value is not a number",
     };
-    return (
+
+    const validateEntry = () => {
+        return !isNaN(numberPeople) && numberPeople > 0 && date && time;
+    }
+
+    const calcPrice = () => {
+      if(useDiscount)
+          return numberPeople * 2 * 0.9
+      else
+          return numberPeople * 2
+    }
+
+    const updateCardPoints = () => {
+      if(useDiscount && cardPoints >= 10)
+          setCardPoints(cardPoints - 10)
+    }
+
+        return (
         <HvContainer>
             <h1>{ name } </h1>
+            <br />
+            <h3>Your card Points -> {cardPoints}</h3>
             <br />
             <h3> Do reservation:</h3>
             <br />
@@ -30,8 +61,32 @@ export const RestaurantDetails = () => {
                 showValidationIcon
             />
             <br />
-            <HvCheckBox label="Use Card points" />
-            <h3>Price {numberPeople * 2}€</h3>
+            <HvDatePicker onChange={() => setDate(true)} label="Pick Date" required id="DatePicker" placeholder="Select date" aria-label="Date" />
+            <br />
+            <HvTimePicker onChange={() => setTime(true)} required id="main" label="Time" placeholder="Select time" locale="pt-pt" />
+            <br />
+            <HvCheckBox disabled={cardPoints < 10} onClick={() => setDiscount(!useDiscount)} label="Use Card points" />
+            <h3>Price {calcPrice()}€</h3>
+            <HvButton onClick={() => {setSuccess(true); updateCardPoints()}} disabled={!validateEntry()} category="primary">
+                Accept
+            </HvButton>
+            <HvDialog
+                disableBackdropClick
+                open={showSuccess}
+                onClose={() => setSuccess(false)}
+                id="dialog"
+                firstFocusable="dialog-close"
+            >
+                <HvDialogTitle variant="info">Success</HvDialogTitle>
+                <HvDialogContent indentContent>
+                    Reservation Completed
+                </HvDialogContent>
+                <HvDialogActions>
+                    <HvButton id="cancel" category="ghost" onClick={() => setSuccess(false)}>
+                        Close
+                    </HvButton>
+                </HvDialogActions>
+            </HvDialog>
         </HvContainer>
     );
 }
